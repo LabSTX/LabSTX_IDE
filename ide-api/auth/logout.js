40c595319d3ee9b23b16c592ca5c4c91e413095e
ProdevappOFFICIAL <122856789+ProdevappOFFICIAL@users.noreply.github.com>
2026-02-25 07:38:05 +0100

    fixed

--- ide-api/auth/logout.js
+++
@@ -1,5 +0,0 @@
-// Logout - Clear GitHub auth cookie
-export default function handler(req, res) {
-    res.setHeader('Set-Cookie', 'github_auth=; Path=/; HttpOnly; SameSite=Lax; Max-Age=0');
-    res.redirect('/');
-}
